42d46075034e52ae9ed68f2261954f2c4056b4f1
Vinicius Gustavo <viniciusgustavodejesus@gmail.com>
2018-11-11 13:54:01 -0200

    Update volume_cilindro_deitado.ino

--- volume_cilindro_deitado.ino
+++ volume_cilindro_deitado.ino
@@ -1,64 +1,94 @@
-
+//Carrega a biblioteca LiquidCrystal
+#include <LiquidCrystal.h>
 #include <Ultrasonic.h>
 #include <math.h>
 
-//Define os pinos para o trigger e echo
-#define pino_trigger 4
-#define pino_echo 5
 
-//Constantes matematicas
-float a,b,c,d,alt,a_base,v;
-float pi = 3.14;
-float r = 1.35;
-float l = 6.06;
-  
+//Define os pinos para o trigger e echo
+#define pino_trigger 6
+#define pino_echo 7
+
 //Inicializa o sensor nos pinos definidos acima
 Ultrasonic ultrasonic(pino_trigger, pino_echo);
-
+ 
+//Define os pinos que serão utilizados para ligação ao display
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+ 
 void setup()
 {
+  //Define o número de colunas e linhas do LCD
+  lcd.begin(16, 2);
   Serial.begin(9600);
-  Serial.println("Lendo dados do sensor...");  
+  Serial.println("Lendo dados do sensor...");
 }
-
+ 
 void loop()
 {
-  float cmMsec, inMsec, meMsec;
+   //Le as informacoes do sensor, em cm e pol
+  float cmMsec, inMsec;
   long microsec = ultrasonic.timing();
-  cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);  
-
-  meMsec = cmMsec/100; // medida em metros
-  alt = (2*r)-meMsec;
- //Calculo da altura do fluido 
+  float a,b,b1,b2,c,d,h;
+  double r = 1.375;
+  float l = 6.250;  
+  double alt, aBase, volume;  
+  double f1, f2; 
   
- a = ((pi*(pow(r,2)))/2);
- b = (pow(r,2))*(asin((r-alt)/alt));
- c = (alt-r);
- d = sqrt((r*r)-((alt-r)*(alt-r)));
+  cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
+  inMsec = ultrasonic.convert(microsec, Ultrasonic::IN);
 
- a_base = (a)-(b)+(c*d); // area da base baseado na integral
+  alt = ((r*2)-(cmMsec/100));
 
- v = a_base*l;
- //Le as informacoes do sensor, em cm e pol
- 
- cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
- inMsec = ultrasonic.convert(microsec, Ultrasonic::IN);
+  b2=0;
+  a = pow(r,2);
+  b = acos (1-(alt/r));
+  c = sqrt ((2*alt*r)-(alt*alt)); 
+  d = (r-alt);
   
- //Exibe informacoes no serial monitor
- Serial.print("DF [cm] ");
- Serial.print(cmMsec);
- Serial.print(" - DF [m] ");
- Serial.print(meMsec);
- Serial.print(" AF [m] ");
- Serial.print(alt);
- Serial.print(" AB ");
- Serial.print(a_base);
- Serial.print(" Volume [m3]");
- Serial.println(v);
- Serial.println(a);
- Serial.println(b);
- Serial.println(c);
- Serial.println(d);
+
+  f1 = PI/180;
+  f2 = 180/PI;
+
+  double  trigo , teste , teta = ((30*PI)/180);    
+  trigo = sin((r-alt)/alt);
+  teste = (r-alt)/alt;
   
- delay(2000);
+  aBase = (a)*(b)-(c*d);
+  volume = (aBase*l);  
+
+  //Serial.print("equacao trigonometrica ");
+  //Serial.println(trigo); 
+  //Serial.print("ardumento da equacao ");
+  //Serial.println(teste);
+  
+  
+  //Exibe informacoes no serial monitor
+  Serial.print("Distancia em cm: ");
+  Serial.println(cmMsec);
+  Serial.print("Altura relativa do Fluido: ");
+  Serial.println(alt);  
+  Serial.print("Termo A: ");
+  Serial.println(a);
+  Serial.println(b);
+  Serial.print("Termo C:");
+  Serial.println(c);
+  Serial.print("Termo D:");
+  Serial.println(d);
+  Serial.print("Area da Base:");
+  Serial.println(aBase);
+  Serial.print("Volume:");
+  Serial.println(volume);
+  Serial.println("================");
+  
+  //Limpa a tela
+  lcd.clear();
+  //Posiciona o cursor na coluna 3, linha 0;
+  lcd.setCursor(3, 0);
+  //Envia o texto entre aspas para o LCD
+  lcd.print("NIVEL=");
+  lcd.print(alt);
+  lcd.setCursor(3, 1);
+  lcd.print("vol=");
+  lcd.print(volume);
+  delay(2000);   
+
 }
